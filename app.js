b76cfbed2fb8bc984906c6c6a5d5e7df1c15ae90
Saurabh kumar <hurtlocker0071@gmail.com>
2019-03-03 06:20:07 +0000

    initia

---
+++ app.js
@@ -0,0 +1,17 @@
+var express= require("express");
+var app = express();
+
+app.get("/",function(res,req){
+   console.log("Active");
+   res.render("animation.ejs");
+});
+
+app.get("*",function(res,req){
+   res.send("<h1>Darling saurabh is working on it.</h1>") 
+});
+
+app.listen(process.env.PORT,process.env.ID,function(){
+   console.log("Working server"); 
+});
+
+
